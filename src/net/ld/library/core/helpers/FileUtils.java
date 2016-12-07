013b601f7f07c9b8c5f875dc0b2df97a4b50d9cd
LintfordPickle <jonnohampson@gmail.com>
2016-12-07 19:03:19 +0100

    FileUtils loadStringFroResource added

--- src/net/ld/library/core/helpers/FileUtils.java
+++ src/net/ld/library/core/helpers/FileUtils.java
@@ -7,31 +7,53 @@ import java.io.InputStreamReader;
 
 public class FileUtils {
 
-	public static String loadAsString(String pFile){
-	
-		System.out.println("Loading as string: " + pFile);
-		
+	/** Loads a binary file as a string, from a file. */
+	public static String loadStringFromFile(String pFile) {
+
 		String lResult = "";
-		try{
-			
-			InputStream lInputStream = FileUtils.class.getResourceAsStream(pFile);
-			
-			BufferedReader lReader = new BufferedReader(new InputStreamReader(lInputStream));
+		try {
+
+			BufferedReader lReader = new BufferedReader(new InputStreamReader(FileUtils.class.getResourceAsStream(pFile)));
 			String lBuffer = "";
-			
-			while((lBuffer = lReader.readLine()) != null){
+
+			while ((lBuffer = lReader.readLine()) != null) {
 				lResult += lBuffer + "\n";
 			}
-			
+
 			lReader.close();
-			
-		}
-		catch(IOException e){
-			System.err.println( "Error loading file as String : " + e );
+
+		} catch (IOException e) {
+			System.err.println("Error loading file as String : " + e);
 			throw new RuntimeException("Error loading file as String");
 		}
-		
+
 		return lResult;
 	}
-	
+
+	/** Loads a binary file as a string, from a resource in the jar file. */
+	public static String loadStringFromResource(String pFile) {
+
+		System.out.println("Loading as string: " + pFile);
+
+		String lResult = "";
+		try {
+
+			InputStream lInputStream = FileUtils.class.getResourceAsStream(pFile);
+
+			BufferedReader lReader = new BufferedReader(new InputStreamReader(lInputStream));
+			String lBuffer = "";
+
+			while ((lBuffer = lReader.readLine()) != null) {
+				lResult += lBuffer + "\n";
+			}
+
+			lReader.close();
+
+		} catch (IOException e) {
+			System.err.println("Error loading file as String : " + e);
+			throw new RuntimeException("Error loading file as String");
+		}
+
+		return lResult;
+	}
 }
