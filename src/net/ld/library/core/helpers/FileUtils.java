738399eb31ae85814c963083721c9fefb528ee06
LintfordPickle <jonnohampson@gmail.com>
2016-12-07 19:03:03 +0100

    FileUtils loadStringFromResource added

--- src/net/ld/library/core/helpers/FileUtils.java
+++ src/net/ld/library/core/helpers/FileUtils.java
@@ -1,17 +1,22 @@
 package net.ld.library.core.helpers;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class FileUtils {
 
 	public static String loadAsString(String pFile){
 	
+		System.out.println("Loading as string: " + pFile);
+		
 		String lResult = "";
 		try{
 			
-			BufferedReader lReader = new BufferedReader(new FileReader(pFile));
+			InputStream lInputStream = FileUtils.class.getResourceAsStream(pFile);
+			
+			BufferedReader lReader = new BufferedReader(new InputStreamReader(lInputStream));
 			String lBuffer = "";
 			
 			while((lBuffer = lReader.readLine()) != null){
@@ -28,4 +33,5 @@ public class FileUtils {
 		
 		return lResult;
 	}
+	
 }
