637c93367386e0248a809e3d82d6149d2763bc7d
Lintford <Lintford@LAPTOP-IK8AS6LD>
2017-04-08 11:08:24 +0200

    Made GameTime internally consistent. Extended unit tests

--- src/net/ld/library/core/time/GameTime.java
+++ src/net/ld/library/core/time/GameTime.java
@@ -1,25 +1,30 @@
 package net.ld.library.core.time;
 
+/**
+ * The {@link GameTime} class is used to track the progression of time
+ * throughout the lifetime of the application. It tracks and recorded the frame
+ * elapsed time as well as the total game time.
+ */
 public class GameTime {
 
-	// =============================================
+	// -------------------------------------
 	// Variables
-	// =============================================
+	// -------------------------------------
 
 	private long mLastFrame;
 	private double mTotalGameTime;
 	private double mElapsedGameTime;
 
-	// =============================================
+	// -------------------------------------
 	// Properties
-	// =============================================
+	// -------------------------------------
 
 	/**
-	 * @return The total game time in seconds.
+	 * @return The total game time in milliseconds.
 	 */
 	public double totalGameTime() {
-		return mTotalGameTime / 1000.0f;
-		
+		return mTotalGameTime;
+
 	}
 
 	/**
@@ -27,32 +32,41 @@ public class GameTime {
 	 */
 	public double elapseGameTime() {
 		return mElapsedGameTime;
-		
+
 	}
 
-	// =============================================
+	// -------------------------------------
 	// Constructor
-	// =============================================
+	// -------------------------------------
 
+	/** public constructor. */
 	public GameTime() {
 		getDelta(); // needs to be called once at least
 
 	}
 
-	// =============================================
+	// -------------------------------------
 	// Core-Methods
-	// =============================================
+	// -------------------------------------
 
+	/**
+	 * update called once per frame. It records the amount of time elapsed since
+	 * the last time update was called.
+	 */
 	public void update() {
 		mElapsedGameTime = getDelta();
 		mTotalGameTime += mElapsedGameTime;
 
 	}
 
-	// =============================================
+	// -------------------------------------
 	// Methods
-	// =============================================
+	// -------------------------------------
 
+	/**
+	 * Returns the delta time (in milliseconds) since the last time this method
+	 * was called.
+	 */
 	private double getDelta() {
 		long time = System.nanoTime();
 		double lDelta = ((time - mLastFrame) / TimeSpan.NanoToMilli);
