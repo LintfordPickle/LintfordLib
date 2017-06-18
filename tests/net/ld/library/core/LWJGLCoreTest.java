fb4b4182a501a0a38b274060f0401947f29ae241
LintfordPickle <jonnohampson@gmail.com>
2017-06-18 12:56:42 +0200

    Restructured core tests

--- tests/net/ld/library/core/LWJGLCoreTest.java
+++ tests/net/ld/library/core/LWJGLCoreTest.java
@@ -3,126 +3,116 @@
  */
 package net.ld.library.core;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import net.ld.library.GameInfo;
 import net.ld.library.core.rendering.RenderState;
 
 /**
- * Tests the core functionality of the abstract {@link LWJGLCore} class when it
- * is extended.
+ * Tests the core functionality of the abstract {@link LWJGLCore} class when it is extended.
  */
 public class LWJGLCoreTest {
 
-	/** Tests the creation of an OpenGL window */
-	@Test
-	public void windowCreatedSuccessfully() {
-		// Arrange (Get all objects ready)
-		LWJGLCore lCoreObject = createLWJGLCoreObject();
+	private static LWJGLCore coreLWJGLObject;
 
-		// Act (perform the action under test)
-		boolean result = lCoreObject.createWindow();
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		// Arrange
+		coreLWJGLObject = createLWJGLCoreObject();
 
-		// Assert (Test the action was performed correctly)
+		// Act
+		boolean result = coreLWJGLObject.createWindow();
+
+		// Assert
 		assert (result) : "LGWJGLCore failed to create window";
 
-		lCoreObject.closeApp();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		// Arrange
+
+		// Act
+		coreLWJGLObject.closeApp();
+
+		// Assert
 
 	}
 
 	/**
-	 * Tests that a valid {@link RenderState} object is instantiated when a
-	 * {@link LWJGLCore} object is created.
+	 * Tests that a valid {@link RenderState} object is instantiated when a {@link LWJGLCore} object is created.
 	 */
 	@Test
 	public void gameTimeCreation() {
-		// Arrange (Get all objects ready)
-		LWJGLCore lCoreObject = createLWJGLCoreObject();
+		// Arrange
 
-		// Act (perform the action under test)
-		lCoreObject.createWindow();
+		// Act
 
 		// Assert (Test the action was performed correctly)
-		assert (lCoreObject.gameTime() != null) : "LGWJGLCore failed to instantiate a valid GameTime object";
-
-		lCoreObject.closeApp();
+		assert (coreLWJGLObject.gameTime() != null) : "LGWJGLCore failed to instantiate a valid GameTime object";
 
 	}
 
 	/**
-	 * Tests that a valid {@link RenderState} object is instantiated when a
-	 * {@link LWJGLCore} object is created.
+	 * Tests that a valid {@link RenderState} object is instantiated when a {@link LWJGLCore} object is created.
 	 */
 	@Test
 	public void inputStateCreation() {
-		// Arrange (Get all objects ready)
-		LWJGLCore lCoreObject = createLWJGLCoreObject();
+		// Arrange
 
-		// Act (perform the action under test)
-		lCoreObject.createWindow();
+		// Act
 
-		// Assert (Test the action was performed correctly)
-		assert (lCoreObject.inputState() != null) : "LGWJGLCore failed to instantiate a valid InputState object";
-
-		lCoreObject.closeApp();
+		// Assert
+		assert (coreLWJGLObject.inputState() != null) : "LGWJGLCore failed to instantiate a valid InputState object";
 
 	}
 
 	/**
-	 * Tests that a valid {@link RenderState} object is instantiated when a
-	 * {@link LWJGLCore} object is created.
+	 * Tests that a valid {@link RenderState} object is instantiated when a {@link LWJGLCore} object is created.
 	 */
 	@Test
 	public void hudCameraCreation() {
-		// Arrange (Get all objects ready)
-		LWJGLCore lCoreObject = createLWJGLCoreObject();
+		// Arrange
 
-		// Act (perform the action under test)
-		lCoreObject.createWindow();
+		// Act
 
-		// Assert (Test the action was performed correctly)
-		assert (lCoreObject.hud() != null) : "LGWJGLCore failed to instantiate a valid HUD object";
-
-		lCoreObject.closeApp();
+		// Assert
+		assert (coreLWJGLObject.hud() != null) : "LGWJGLCore failed to instantiate a valid HUD object";
 
 	}
 
 	/**
-	 * Tests that a valid {@link RenderState} object is instantiated when a
-	 * {@link LWJGLCore} object is created.
+	 * Tests that a valid {@link RenderState} object is instantiated when a {@link LWJGLCore} object is created.
 	 */
 	@Test
 	public void gameCameraCreation() {
-		// Arrange (Get all objects ready)
-		LWJGLCore lCoreObject = createLWJGLCoreObject();
+		// Arrange
 
-		// Act (perform the action under test)
-		lCoreObject.createWindow();
+		// Act
 
-		// Assert (Test the action was performed correctly)
-		assert (lCoreObject.gameCamera() != null) : "LGWJGLCore failed to instantiate a valid Camera object";
+		// Assert
+		assert (coreLWJGLObject.gameCamera() != null) : "LGWJGLCore failed to instantiate a valid Camera object";
 
 	}
 
 	/**
-	 * Tests that a valid {@link RenderState} object is instantiated when a
-	 * {@link LWJGLCore} object is created.
+	 * Tests that a valid {@link RenderState} object is instantiated when a {@link LWJGLCore} object is created.
 	 */
 	@Test
 	public void renderStateCreation() {
-		// Arrange (Get all objects ready)
-		LWJGLCore lCoreObject = createLWJGLCoreObject();
+		// Arrange
 
-		// Act (perform the action under test)
-		lCoreObject.createWindow();
+		// Act
 
-		// Assert (Test the action was performed correctly)
-		assert (lCoreObject.renderState() != null) : "LGWJGLCore failed to instantiate a valid RenderState object";
+		// Assert
+		assert (coreLWJGLObject.renderState() != null) : "LGWJGLCore failed to instantiate a valid RenderState object";
 
 	}
 
 	/** Creates a new instance of LWJGLCore and returns it. */
-	private LWJGLCore createLWJGLCoreObject() {
+	private static LWJGLCore createLWJGLCoreObject() {
 		final GameInfo GAME_INFO = new GameInfo() {
 		};
 		LWJGLCore testObject = new LWJGLCore(GAME_INFO) {
