1bae3446edc5300453853c7c7c1a5c9e7d5886cb
LintfordPickle <jonnohampson@gmail.com>
2017-06-24 01:55:12 +0200

    Added meaning console output

--- src/net/ld/library/core/helpers/FileUtils.java
+++ src/net/ld/library/core/helpers/FileUtils.java
@@ -2,6 +2,7 @@ package net.ld.library.core.helpers;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,29 +11,34 @@ import java.io.InputStreamReader;
 public class FileUtils {
 
 	/** Loads a binary file as a string, from a file. */
-	public static String loadStringFromFile(String pFile) {
+	public static String loadStringFromFile(String pFilepath) throws FileNotFoundException, IOException {
 
-		File lFile = new File(pFile);
-		if(!lFile.exists()){
-			throw new RuntimeException("FFS - the file doesn't exist!");
-			
+		System.out.println("Loading file as string: " + pFilepath);
+
+		File lFile = new File(pFilepath);
+		if (!lFile.exists()) {
+			throw new FileNotFoundException("Cannot find the specified file (" + pFilepath + ")!");
+
 		}
-		
-		String lResult = "";
-		try {
 
-			BufferedReader lReader = new BufferedReader(new FileReader(lFile));
+		String lResult = "";
+		BufferedReader lReader = null;
+
+		try {
+			lReader = new BufferedReader(new FileReader(lFile));
 			String lBuffer = "";
 
 			while ((lBuffer = lReader.readLine()) != null) {
 				lResult += lBuffer + "\n";
+
 			}
 
-			lReader.close();
+		} finally {
+			if (lReader != null) {
+				lReader.close();
 
-		} catch (IOException e) {
-			System.err.println("Error loading file as String : " + e);
-			throw new RuntimeException("Error loading file as String");
+			}
+
 		}
 
 		return lResult;
@@ -41,7 +47,7 @@ public class FileUtils {
 	/** Loads a binary file as a string, from a resource in the jar file. */
 	public static String loadStringFromResource(String pFile) {
 
-		System.out.println("Loading as string: " + pFile);
+		System.out.println("Loading resource as string: " + pFile);
 
 		String lResult = "";
 		try {
@@ -64,4 +70,5 @@ public class FileUtils {
 
 		return lResult;
 	}
+
 }
