cc28ffcd8c35954ba4cf5dae89f9baefb044dd62
LintfordPickle <jonnohampson@gmail.com>
2018-08-19 01:41:40 +0200

    Added Joystick skeletal code (not finished)

--- src/net/lintford/library/core/input/InputState.java
+++ src/net/lintford/library/core/input/InputState.java
@@ -1,10 +1,13 @@
 package net.lintford.library.core.input;
 
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
 import java.util.Arrays;
 
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWCharModsCallback;
 import org.lwjgl.glfw.GLFWCursorPosCallback;
+import org.lwjgl.glfw.GLFWJoystickCallback;
 import org.lwjgl.glfw.GLFWKeyCallback;
 import org.lwjgl.glfw.GLFWMouseButtonCallback;
 import org.lwjgl.glfw.GLFWScrollCallback;
@@ -13,6 +16,63 @@ import net.lintford.library.core.LintfordCore;
 import net.lintford.library.core.maths.Vector2f;
 
 public class InputState {
+
+	public class LintfordJoystick extends GLFWJoystickCallback {
+		private int mJoystickIndex;
+		private ByteBuffer mJoystickButtons;
+		private FloatBuffer mJoystickAxes;
+		private String mJoystickName;
+		private boolean mIsActive;
+
+		public boolean isActive() {
+			return mIsActive;
+		}
+
+		public String name() {
+			return mJoystickName;
+		}
+
+		// --------------------------------------
+		// Constructor
+		// --------------------------------------
+
+		public LintfordJoystick(final int pIndex) {
+			mJoystickIndex = pIndex;
+
+			GLFW.glfwSetJoystickCallback(this);
+
+		}
+
+		// --------------------------------------
+		// Methods
+		// --------------------------------------
+
+		public void update(LintfordCore pCore) {
+			mJoystickButtons = GLFW.glfwGetJoystickButtons(mJoystickIndex);
+			mJoystickAxes = GLFW.glfwGetJoystickAxes(mJoystickIndex);
+
+		}
+
+		public void reset() {
+
+		}
+
+		void joystick_callback(int joy, int event) {
+			if (event == GLFW.GLFW_CONNECTED) {
+				mIsActive = true;
+
+			} else if (event == GLFW.GLFW_DISCONNECTED) {
+				mIsActive = false;
+
+			}
+		}
+
+		@Override
+		public void invoke(int arg0, int arg1) {
+
+		}
+
+	}
 
 	public class KeyCallback extends GLFWKeyCallback {
 
@@ -407,6 +467,8 @@ public class InputState {
 		mKeyTimer += lDeltaTime;
 		mMenuClickTimer += lDeltaTime;
 
+		handleJoysticks();
+
 		if (isMouseTimedLeftClickAvailable() && mCaptureKeyboardInput) {
 			stopCapture();
 
@@ -450,6 +512,13 @@ public class InputState {
 	// --------------------------------------
 	// Methods
 	// --------------------------------------
+
+	private void handleJoysticks() {
+		ByteBuffer d = GLFW.glfwGetJoystickButtons(GLFW.GLFW_JOYSTICK_1);
+		FloatBuffer lAxes = GLFW.glfwGetJoystickAxes(GLFW.GLFW_JOYSTICK_1);
+		String lJoystickName = GLFW.glfwGetJoystickName(GLFW.GLFW_JOYSTICK_1);
+
+	}
 
 	public boolean tryAquireLeftClickOwnership(int pHash) {
 		if (!mouseLeftClick())
