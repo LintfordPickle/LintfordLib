3335aca4e1b1bdcf9a86e56f0b97b701b327ca4f
LintfordPickle <jonnohampson@gmail.com>
2018-10-09 20:50:46 +0200

    Added copyFolder method. Extended DebugMemory

--- src/net/lintford/library/core/debug/DebugMemory.java
+++ src/net/lintford/library/core/debug/DebugMemory.java
@@ -1,22 +1,101 @@
 package net.lintford.library.core.debug;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
 public class DebugMemory {
+
+	// --------------------------------------
+	// Constants
+	// --------------------------------------
+
+	private static final long MEGABYTE_FACTOR = 1024L * 1024L;
+	private static final DecimalFormat ROUNDED_DOUBLE_DECIMALFORMAT;
+	private static final String MIB = "MiB";
+
+	static {
+		DecimalFormatSymbols otherSymbols = new DecimalFormatSymbols(Locale.ENGLISH);
+		otherSymbols.setDecimalSeparator('.');
+		otherSymbols.setGroupingSeparator(',');
+		ROUNDED_DOUBLE_DECIMALFORMAT = new DecimalFormat("####0.00", otherSymbols);
+		ROUNDED_DOUBLE_DECIMALFORMAT.setGroupingUsed(false);
+	}
+
+	// --------------------------------------
+	// Static Methods
+	// --------------------------------------
 
 	public static void dumpMemoryToLog() {
 		if (!Debug.debugManager().debugModeEnabled())
 			return;
-		
+
 		final DebugLogger LOGGER = Debug.debugManager().logger();
 
-		// Returns the maximum amount of memory that the JVM will attempt to use
-		LOGGER.i(DebugMemory.class.getSimpleName(), "Maximum memory (bytes): " + Runtime.getRuntime().maxMemory());
+		// Returns the maximum amount of memory that the JVM will attempt to use (VM argument -Xmx512M)
+		LOGGER.i(DebugMemory.class.getSimpleName(), "Maximum memory: " + getMaxMemoryInMiB());
 
-		// Returns the total amount of memory in the JVM
-		LOGGER.i(DebugMemory.class.getSimpleName(), "Total memory (bytes): " + Runtime.getRuntime().totalMemory());
+		// Returns the total amount of memory in the JVM (influenced with VM argument -Xms256M)
+		LOGGER.i(DebugMemory.class.getSimpleName(), "Total memory: " + getTotalMemoryInMiB());
 
 		// Returns the amount of free memory in the JVM
-		LOGGER.i(DebugMemory.class.getSimpleName(), "Free memory (bytes): " + Runtime.getRuntime().freeMemory());
+		LOGGER.i(DebugMemory.class.getSimpleName(), "Free memory: " + getFreeMemoryInMiB());
 
+		LOGGER.i(DebugMemory.class.getSimpleName(), "Used memory: " + getUsedMemoryInMiB());
+
+	}
+
+	// --------------------------------------
+	// Methods
+	// --------------------------------------
+
+	private static double bytesToMiB(long bytes) {
+		return ((double) bytes / MEGABYTE_FACTOR);
+	}
+
+	public static String getTotalMemoryInMiB() {
+		double totalMiB = bytesToMiB(getTotalMemory());
+		return String.format("%s %s", ROUNDED_DOUBLE_DECIMALFORMAT.format(totalMiB), MIB);
+	}
+
+	public static String getFreeMemoryInMiB() {
+		double freeMiB = bytesToMiB(getFreeMemory());
+		return String.format("%s %s", ROUNDED_DOUBLE_DECIMALFORMAT.format(freeMiB), MIB);
+	}
+
+	public static String getUsedMemoryInMiB() {
+		double usedMiB = bytesToMiB(getUsedMemory());
+		return String.format("%s %s", ROUNDED_DOUBLE_DECIMALFORMAT.format(usedMiB), MIB);
+	}
+
+	public static String getMaxMemoryInMiB() {
+		double maxMiB = bytesToMiB(getMaxMemory());
+		return String.format("%s %s", ROUNDED_DOUBLE_DECIMALFORMAT.format(maxMiB), MIB);
+	}
+
+	public static double getPercentageUsed() {
+		return ((double) getUsedMemory() / getMaxMemory()) * 100;
+	}
+
+	public static String getPercentageUsedFormatted() {
+		double usedPercentage = getPercentageUsed();
+		return ROUNDED_DOUBLE_DECIMALFORMAT.format(usedPercentage) + "%";
+	}
+
+	public static long getMaxMemory() {
+		return Runtime.getRuntime().maxMemory();
+	}
+
+	public static long getUsedMemory() {
+		return getMaxMemory() - getFreeMemory();
+	}
+
+	public static long getTotalMemory() {
+		return Runtime.getRuntime().totalMemory();
+	}
+
+	public static long getFreeMemory() {
+		return Runtime.getRuntime().freeMemory();
 	}
 
 }
--- src/net/lintford/library/core/storage/FileUtils.java
+++ src/net/lintford/library/core/storage/FileUtils.java
@@ -1,10 +1,14 @@
 package net.lintford.library.core.storage;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 
 public class FileUtils {
 
@@ -71,6 +75,54 @@ public class FileUtils {
 		}
 
 		return lResult;
+	}
+
+	/** Copies the source folder, and all its contents, to the destination folder. The destination folder is created if it doesn't already exist. */
+	public static void copyFolder(File source, File destination) {
+		if (source.isDirectory()) {
+			if (!destination.exists()) {
+				destination.mkdirs();
+			}
+
+			String files[] = source.list();
+
+			for (String file : files) {
+				File srcFile = new File(source, file);
+				File destFile = new File(destination, file);
+
+				copyFolder(srcFile, destFile);
+			}
+			
+		} else {
+			InputStream in = null;
+			OutputStream out = null;
+
+			try {
+				in = new FileInputStream(source);
+				out = new FileOutputStream(destination);
+
+				byte[] buffer = new byte[1024];
+
+				int length;
+				while ((length = in.read(buffer)) > 0) {
+					out.write(buffer, 0, length);
+				}
+			} catch (Exception e) {
+				try {
+					if(in != null)
+						in.close();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+
+				try {
+					if(out != null)
+						out.close();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+		}
 	}
 
 }
