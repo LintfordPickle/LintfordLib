0201b1457b7d38fe090fd9f4f7500a94c74da3e1
LintfordPickle <LintfordPickle@DESKTOP-81JTO4M.Speedport_W_724V_Typ_A_05011603_06_001>
2019-06-20 15:23:58 +0200

    removed memory allocations from SAT collision checks

--- src/net/lintford/library/core/collisions/SAT.java
+++ src/net/lintford/library/core/collisions/SAT.java
@@ -6,54 +6,67 @@ import net.lintford.library.core.maths.Vector2f;
 
 public class SAT {
 
+	private static Vector2f mTempVector2f0 = new Vector2f();
+	private static Vector2f mTempVector2f1 = new Vector2f();
+
+	private static Vector2f[] mTempAxes0 = new Vector2f[] { new Vector2f(0, 0), new Vector2f(0, 0) };
+	private static Vector2f[] mTempAxes1 = new Vector2f[] { new Vector2f(0, 0), new Vector2f(0, 0) };
+
 	public static boolean intersects(Shape pShape0, Shape pShape1) {
+		if (pShape0 == null || pShape1 == null)
+			return false;
+
 		Vector2f[] axes0 = pShape0.getAxes();
 		Vector2f[] axes1 = pShape1.getAxes();
 
-		if(axes0 == null && pShape0 instanceof Circle) {
-			axes0 = new Vector2f[1];
-			axes0[0] = ((Circle)pShape0).getNearestVertex(pShape1, new Vector2f());
+		if (axes0 == null && pShape0 instanceof Circle) {
+			axes0 = mTempAxes0;
+			mTempVector2f0 = ((Circle) pShape0).getNearestVertex(pShape1, mTempVector2f0);
+			axes0[0].x = mTempVector2f0.x;
+			axes0[0].y = mTempVector2f0.y;
 		}
-		
-		if(axes1 == null && pShape1 instanceof Circle) {
-			axes1 = new Vector2f[1];
-			axes1[0] = ((Circle)pShape1).getNearestVertex(pShape0, new Vector2f());
+
+		if (axes1 == null && pShape1 instanceof Circle) {
+			axes1 = mTempAxes1;
+			mTempVector2f0 = ((Circle) pShape1).getNearestVertex(pShape0, mTempVector2f0);
+			axes1[0].x = mTempVector2f0.x;
+			axes1[0].y = mTempVector2f0.y;
 		}
-		
+
 		// loop over the axes0
-		if(axes0 != null) {
+		if (axes0 != null) {
 			for (int i = 0; i < axes0.length; i++) {
 				Vector2f axis = axes0[i];
-				
+
 				// project both shapes onto the axis
-				Vector2f p1 = pShape0.project(axis, new Vector2f());
-				Vector2f p2 = pShape1.project(axis, new Vector2f());
-				
+				mTempVector2f0 = pShape0.project(axis, mTempVector2f0);
+				mTempVector2f1 = pShape1.project(axis, mTempVector2f1);
+
 				// do the projections overlap?
-				if (!overlaps(p1, p2)) {
+				if (!overlaps(mTempVector2f0, mTempVector2f1)) {
 					// then we can guarantee that the shapes do not overlap
 					return false;
 				}
 			}
-			
+
 		}
 
 		// loop over the axes1
-		if(axes1 != null) {
+		if (axes1 != null) {
 			for (int i = 0; i < axes1.length; i++) {
 				Vector2f axis = axes1[i];
-				
+
 				// project both shapes onto the axis
-				Vector2f p1 = pShape0.project(axis, new Vector2f());
-				Vector2f p2 = pShape1.project(axis, new Vector2f());
-				
+				mTempVector2f0 = pShape0.project(axis, mTempVector2f0);
+				mTempVector2f1 = pShape1.project(axis, mTempVector2f1);
+
 				// do the projections overlap?
-				if (!overlaps(p1, p2)) {
+				if (!overlaps(mTempVector2f0, mTempVector2f1)) {
 					// then we can guarantee that the shapes do not overlap
 					return false;
 				}
 			}
-			
+
 		}
 
 		// If we can get here then we know that every axis had overlap on it
