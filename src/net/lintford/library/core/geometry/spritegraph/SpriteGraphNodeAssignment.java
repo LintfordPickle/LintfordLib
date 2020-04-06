6105cd29b530c7dac7e5d3991d9eab34e948a430
John Hampson <jonnohampson@gmail.com>
2020-04-06 20:45:54 +0200

    removed unused files

--- src/net/lintford/library/core/geometry/spritegraph/SpriteGraphNodeAssignment.java
+++
@@ -1,9 +0,0 @@
-package net.lintford.library.core.geometry.spritegraph;
-
-import net.lintford.library.core.geometry.spritegraph.instance.SpriteGraphNodeInstance;
-
-public class SpriteGraphNodeAssignment {
-
-	public SpriteGraphNodeInstance mSpriteGraphNodeInstance;
-	public ISpriteGraphNodeAssignment mAssignedItem;
-}
