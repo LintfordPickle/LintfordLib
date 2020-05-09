9bc7b1eeba63ea3757e19c1c8797ea9a0b66460f
lintfordpickle <jonnohampson@gmail.com>
2020-05-09 18:05:28 +0200

    Added dedicated physics constants class

---
+++ src/net/lintford/library/ConstantsPhysics.java
@@ -0,0 +1,42 @@
+package net.lintford.library;
+
+public class ConstantsPhysics {
+
+	// --------------------------------------
+	// Statics
+	// --------------------------------------
+
+	private static float UNITS_TO_PIXELS = 16f;
+	private static float PIXELS_TO_UNITS = 1f / UNITS_TO_PIXELS;
+
+	// --------------------------------------
+	// Properties
+	// --------------------------------------
+
+	public static float UnitsToPixels() {
+		return UNITS_TO_PIXELS;
+	}
+
+	public static float PixelsToUnits() {
+		return PIXELS_TO_UNITS;
+	}
+
+	public static float toPixels(float pUnits) {
+		return UNITS_TO_PIXELS * pUnits;
+	}
+
+	public static float toUnits(float pPixels) {
+		return PIXELS_TO_UNITS * pPixels;
+	}
+
+	// --------------------------------------
+	// Methods
+	// --------------------------------------
+
+	public void setPhysicsWorldConstants(float pNumPixelsPerUnit) {
+		UNITS_TO_PIXELS = pNumPixelsPerUnit;
+		PIXELS_TO_UNITS = 1f / UNITS_TO_PIXELS;
+
+	}
+
+}
