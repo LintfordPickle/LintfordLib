632cda44dac6390b39bec51b7a243d986fb12bc3
John Hampson <john.hampson@jjk.de>
2020-08-31 22:29:41 +0200

    updated particle emitter meta data filename

--- src/net/lintford/library/core/particles/particleemitters/ParticleEmitterConstants.java
+++ src/net/lintford/library/core/particles/particleemitters/ParticleEmitterConstants.java
@@ -1,5 +1,5 @@
 package net.lintford.library.core.particles.particleemitters;
 
 public class ParticleEmitterConstants {
-	public static final String PARTICLE_EMITTER_META_FILENAME = "res//def//particles//emitters//emitters_meta.json";
+	public static final String PARTICLE_EMITTER_META_FILENAME = "res//def//particles//emitters//_meta.json";
 }
