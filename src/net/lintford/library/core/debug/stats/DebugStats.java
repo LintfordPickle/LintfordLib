d15af9f8b48cd9a4d5463c59a82e4ebf409b8288
LintfordPickle <jonnohampson@gmail.com>
2022-09-30 19:42:45 +0200

    updated debug stats with resolution and window size

--- src/net/lintford/library/core/debug/stats/DebugStats.java
+++ src/net/lintford/library/core/debug/stats/DebugStats.java
@@ -41,6 +41,7 @@ public class DebugStats extends Rectangle implements IScrollBarArea, IProcessMou
 	public static final int TAG_ID_TEXTURES = 5;
 	public static final int TAG_ID_RENDERTEXTURES = 6;
 	public static final int TAG_ID_RES = 7;
+	public static final int TAG_ID_WINDOW_SIZE = 15;
 	public static final int TAG_ID_VRAM = 8;
 	public static final int TAG_ID_VBO = 9;
 	public static final int TAG_ID_VB_UPLOADS = 10;
@@ -112,6 +113,7 @@ public class DebugStats extends Rectangle implements IScrollBarArea, IProcessMou
 		mTags.add(new DebugStatTagString(TAG_ID_TOTAL_ELAPSED_TIME_MS, "Up (ms)", ""));
 		mTags.add(new DebugStatTagString(TAG_ID_TIMESTEP, "Timestep", ""));
 		mTags.add(new DebugStatTagString(TAG_ID_RES, "Resolution", ""));
+		mTags.add(new DebugStatTagString(TAG_ID_WINDOW_SIZE, "Window", ""));
 		// mTags.add(new DebugStatTagFloat(-1, "Ram Used", 0, false));
 		// mTags.add(new DebugStatTagFloat(-1, "Ram Free", 0, false));
 		// mTags.add(new DebugStatTagFloat(TAG_ID_VRAM, "VRam", 0, false));
@@ -261,6 +263,16 @@ public class DebugStats extends Rectangle implements IScrollBarArea, IProcessMou
 		if (!mDebugManager.debugManagerEnabled())
 			return;
 
+		if (!mIsOpen)
+			return;
+
+		final var lCamWidth = (int) core.gameCamera().getWidth();
+		final var lCamHeight = (int) core.gameCamera().getHeight();
+
+		Debug.debugManager().stats().setTagValue(DebugStats.TAG_ID_RES, lCamWidth + "x" + lCamHeight);
+		final var lDisplaySettings = core.config().display();
+		Debug.debugManager().stats().setTagValue(DebugStats.TAG_ID_WINDOW_SIZE, lDisplaySettings.windowWidth() + "x" + lDisplaySettings.windowHeight());
+
 		mDeltaFrameCount++;
 
 		mTimer += core.appTime().elapsedTimeMilli();
@@ -273,9 +285,6 @@ public class DebugStats extends Rectangle implements IScrollBarArea, IProcessMou
 		Debug.debugManager().stats().setTagValue(DebugStats.TAG_ID_FPS, mFrameCount);
 
 		mLastDrawElapsed = core.appTime().elapsedTimeMilli();
-
-		if (!mIsOpen)
-			return;
 
 		mSpriteBatch.begin(core.HUD());
 		mConsoleFont.begin(core.HUD());
