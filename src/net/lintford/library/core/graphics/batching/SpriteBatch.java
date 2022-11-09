9f5f58dd7b07c3c82c43dd7318feae568f8489bb
LintfordPickle <jonnohampson@gmail.com>
2022-11-09 22:52:12 +0100

    added overloaded method for rotated sprite rendering

--- src/net/lintford/library/core/graphics/batching/SpriteBatch.java
+++ src/net/lintford/library/core/graphics/batching/SpriteBatch.java
@@ -92,8 +92,11 @@ public class SpriteBatch extends TextureBatchPCT {
 		draw(lTexture, lCurrentSpriteFrame, destRectangle, zDepth, colorTint);
 	}
 
-	public void drawAroundCenter(SpriteSheetDefinition spriteSheetDefinition, SpriteFrame spriteFrame, float destX, float destY, float destWidth, float destHeight, float rot, float pivotX, float pivotY, float zDepth, Color colorTint) {
+	public void drawAroundCenter(SpriteSheetDefinition spriteSheetDefinition, int spriteFrameIndex, float destX, float destY, float destWidth, float destHeight, float rot, float pivotX, float pivotY, float zDepth, Color colorTint) {
+		drawAroundCenter(spriteSheetDefinition, spriteSheetDefinition.getSpriteFrame(spriteFrameIndex), destX, destY, destWidth, destHeight, rot, pivotX, pivotY, zDepth, colorTint);
+	}
 
+	public void drawAroundCenter(SpriteSheetDefinition spriteSheetDefinition, SpriteFrame spriteFrame, float destX, float destY, float destWidth, float destHeight, float rot, float pivotX, float pivotY, float zDepth, Color colorTint) {
 		drawAroundCenter(spriteSheetDefinition.texture(), spriteFrame.x(), spriteFrame.y(), spriteFrame.width(), spriteFrame.height(), destX, destY, destWidth, destHeight, -0.01f, rot, pivotX, pivotY, 1.f, colorTint);
 	}
 }
