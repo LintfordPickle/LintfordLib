47d95eeaae69534c2ded63411a7281593fcafdfc
LintfordPickle <jonnohampson@gmail.com>
2023-01-03 22:18:43 +0100

    separated narrow/broad phase

--- src/net/lintford/library/core/collisions/PhysicsWorld.java
+++ src/net/lintford/library/core/collisions/PhysicsWorld.java
@@ -16,27 +16,36 @@ import net.lintford.library.core.time.TimeConstants;
 public class PhysicsWorld {
 
 	// --------------------------------------
+	// Inner-Classes
+	// --------------------------------------
+
+	private class CollisionPair {
+		public int bodyAUid;
+		public int bodyBUid;
+	}
+
+	// --------------------------------------
 	// Constants
 	// --------------------------------------
 
-	private float mGravityX;
-	private float mGravityY; // mps/s
+	private final ContactManifold mContactManifold = new ContactManifold();
 
 	// --------------------------------------
 	// Variables
 	// --------------------------------------
 
+	private float mGravityX;
+	private float mGravityY; // mps/s
+
 	private final List<RigidBody> mBodies = new ArrayList<>();
 
-	private final LinkedList<ContactManifold> mContactPool;
-	private final List<ContactManifold> mContacts = new ArrayList<>(64);
-	public final List<ContactManifold> mActiveContactsManifoldsList = new ArrayList<>(64);
+	private final LinkedList<CollisionPair> mCollisionPairPool = new LinkedList<>();
+	private final List<CollisionPair> mCollisionPair = new ArrayList<>(16);
 
 	private DebugStatTagCaption mDebugStatPhysicsCaption;
 	private DebugStatTagInt mDebugStatsNumBodies;
 	private DebugStatTagFloat mDebugStepTimeInMm;
 	private DebugStatTagInt mDebugNumIterations;
-	private DebugStatTagInt mDebugNumContacts;
 
 	private boolean mInitialized = false;
 
@@ -63,10 +72,6 @@ public class PhysicsWorld {
 	public PhysicsWorld(float gravityX, float gravityY) {
 		mGravityX = gravityX;
 		mGravityY = gravityY;
-
-		mContactPool = new LinkedList<>();
-
-		createContacts(128);
 	}
 
 	// --------------------------------------
@@ -74,20 +79,26 @@ public class PhysicsWorld {
 	// --------------------------------------
 
 	public void initialize() {
+		initializeCollisionPairPool();
+
 		// TODO: Only do this if in debug mode
 		mDebugStatPhysicsCaption = new DebugStatTagCaption("Physics");
 		mDebugStatsNumBodies = new DebugStatTagInt("Num Bodies", 0, false);
 		mDebugStepTimeInMm = new DebugStatTagFloat("step", 0.0f, false);
 		mDebugNumIterations = new DebugStatTagInt("Num Iterations", 0, false);
-		mDebugNumContacts = new DebugStatTagInt("Num Contacts", 0, false);
 
 		Debug.debugManager().stats().addCustomStatTag(mDebugStatPhysicsCaption);
 		Debug.debugManager().stats().addCustomStatTag(mDebugStatsNumBodies);
 		Debug.debugManager().stats().addCustomStatTag(mDebugStepTimeInMm);
 		Debug.debugManager().stats().addCustomStatTag(mDebugNumIterations);
-		Debug.debugManager().stats().addCustomStatTag(mDebugNumContacts);
 
 		mInitialized = true;
+	}
+
+	private void initializeCollisionPairPool() {
+		for (int i = 0; i < 32; i++) {
+			mCollisionPairPool.addLast(new CollisionPair());
+		}
 	}
 
 	public void unload() {
@@ -95,7 +106,6 @@ public class PhysicsWorld {
 		Debug.debugManager().stats().removeCustomStatTag(mDebugStatsNumBodies);
 		Debug.debugManager().stats().removeCustomStatTag(mDebugStepTimeInMm);
 		Debug.debugManager().stats().removeCustomStatTag(mDebugNumIterations);
-		Debug.debugManager().stats().removeCustomStatTag(mDebugNumContacts);
 
 		mDebugStatPhysicsCaption = null;
 		mDebugStatsNumBodies = null;
@@ -105,81 +115,103 @@ public class PhysicsWorld {
 		mInitialized = false;
 	}
 
-	public void step(float time, int iterations) {
-		mActiveContactsManifoldsList.clear();
-
+	public void stepWorld(float time, int totalIterations) {
 		if (mInitialized == false) {
 			Debug.debugManager().logger().w(getClass().getSimpleName(), "Cannot step physics world - not initialized");
 			return;
 		}
 
-		iterations = MathHelper.clampi(iterations, ConstantsPhysics.MIN_ITERATIONS, ConstantsPhysics.MAX_ITERATIONS);
-		mDebugNumIterations.setValue(iterations);
+		totalIterations = MathHelper.clampi(totalIterations, ConstantsPhysics.MIN_ITERATIONS, ConstantsPhysics.MAX_ITERATIONS);
+		mDebugNumIterations.setValue(totalIterations);
 		mDebugStatsNumBodies.setValue(numBodies());
 
 		final var lSystemTimeBegin = System.nanoTime();
 
-		time /= (float) iterations;
-		for (int it = 0; it < iterations; it++) {
+		time /= (float) totalIterations;
+		for (int it = 0; it < totalIterations; it++) {
 
-			// 1. Movement
-			final int lNumBodies = mBodies.size();
-			for (int i = 0; i < lNumBodies; i++) {
-				mBodies.get(i).step(time, mGravityX, mGravityY);
-			}
+			stepBodies(time);
 
-			var lManifold = getFreeContactManifold();
+			broadPhase();
 
-			// 2. Collision (Broad phase)
-			for (int i = 0; i < lNumBodies - 1; i++) {
-				final var lBodyA = mBodies.get(i);
-				final var lBodyA_aabb = lBodyA.aabb();
-
-				for (int j = i + 1; j < lNumBodies; j++) {
-					final var lBodyB = mBodies.get(j);
-					final var lBodyB_aabb = lBodyB.aabb();
-
-					if (lBodyA.isStatic() && lBodyB.isStatic())
-						continue;
-
-					if (lBodyA_aabb.intersectsAA(lBodyB_aabb) == false)
-						continue;
-
-					// TODO: Move to narrow phase
-					if (SAT.checkCollides(lBodyA, lBodyB, lManifold)) {
-
-						separateBodiesByMTV(lBodyA, lBodyB, lManifold.mtv.x * lManifold.depth, lManifold.mtv.y * lManifold.depth);
-
-						SAT.fillContactPoints(lManifold);
-
-						mContacts.add(lManifold);
-						lManifold = getFreeContactManifold();
-					}
-				}
-			}
-
-			mDebugNumContacts.setValue(mContacts.size());
-
-			// 3. Resolution (Narrow Phase)
-			final var lNumContacts = mContacts.size();
-			for (int i = 0; i < lNumContacts; i++) {
-				final var lNextCollision = mContacts.get(i);
-				resolveCollision(lNextCollision.bodyA, lNextCollision.bodyB, lNextCollision);
-
-				if (it == iterations - 1) {
-
-					// Add contact points for debug display
-					if (mActiveContactsManifoldsList.contains(lManifold) == false)
-						mActiveContactsManifoldsList.add(lManifold);
-				}
-			}
-
-			mContactPool.addAll(mContacts);
-			mContacts.clear();
+			narrowPhase();
 		}
 
 		final var lDelta = ((System.nanoTime() - lSystemTimeBegin) / TimeConstants.NanoToMilli);
 		mDebugStepTimeInMm.setValue((float) lDelta);
+	}
+
+	private void stepBodies(float time) {
+		final int lNumBodies = mBodies.size();
+		for (int i = 0; i < lNumBodies; i++) {
+			mBodies.get(i).step(time, mGravityX, mGravityY);
+		}
+	}
+
+	private void broadPhase() {
+		final var lNumBodies = mBodies.size();
+		for (int i = 0; i < lNumBodies - 1; i++) {
+			final var lBodyA = mBodies.get(i);
+			final var lBodyA_aabb = lBodyA.aabb();
+
+			for (int j = i + 1; j < lNumBodies; j++) {
+				final var lBodyB = mBodies.get(j);
+				final var lBodyB_aabb = lBodyB.aabb();
+
+				if (lBodyA.isStatic() && lBodyB.isStatic())
+					continue;
+
+				if (lBodyA_aabb.intersectsAA(lBodyB_aabb) == false)
+					continue;
+
+				final var lCollisionPair = getFreeCollisionPair();
+				lCollisionPair.bodyAUid = i;
+				lCollisionPair.bodyBUid = j;
+
+				mCollisionPair.add(lCollisionPair);
+			}
+		}
+	}
+
+	private void narrowPhase() {
+		final var lNumCollisionPairs = mCollisionPair.size();
+		for (int i = 0; i < lNumCollisionPairs; i++) {
+			final var lCollisionPair = mCollisionPair.get(i);
+
+			final var lBodyA = mBodies.get(lCollisionPair.bodyAUid);
+			final var lBodyB = mBodies.get(lCollisionPair.bodyBUid);
+
+			if (SAT.checkCollides(lBodyA, lBodyB, mContactManifold)) {
+				separateBodiesByMTV(lBodyA, lBodyB, mContactManifold.mtv.x * mContactManifold.depth, mContactManifold.mtv.y * mContactManifold.depth);
+
+				SAT.fillContactPoints(mContactManifold);
+
+				final float relVelX = lBodyB.vx - lBodyA.vx;
+				final float relVelY = lBodyB.vy - lBodyA.vy;
+
+				final float dotVelNor = Vector2f.dot(relVelX, relVelY, mContactManifold.mtv.x, mContactManifold.mtv.y);
+
+				if (dotVelNor < 0.f) {
+					final float minRestitution = Math.min(lBodyA.restitution(), lBodyB.restitution());
+					float j = -(1.f + minRestitution) * dotVelNor;
+
+					j /= (lBodyA.invMass() + lBodyB.invMass());
+
+					final float impulseX = j * mContactManifold.mtv.x;
+					final float impulseY = j * mContactManifold.mtv.y;
+
+					lBodyA.vx -= impulseX * lBodyA.invMass();
+					lBodyA.vy -= impulseY * lBodyA.invMass();
+
+					lBodyB.vx += impulseX * lBodyB.invMass();
+					lBodyB.vy += impulseY * lBodyB.invMass();
+				}
+			}
+
+			returnCollisionPair(lCollisionPair);
+		}
+
+		mCollisionPair.clear();
 	}
 
 	private void separateBodiesByMTV(final RigidBody lBodyA, final RigidBody lBodyB, float mtvX, float mtvY) {
@@ -196,30 +228,6 @@ public class PhysicsWorld {
 			lBodyB.x += mtvX / 2.f;
 			lBodyB.y += mtvY / 2.f;
 		}
-	}
-
-	private void resolveCollision(RigidBody bodyA, RigidBody bodyB, ContactManifold manifold) {
-		final float relVelX = bodyB.vx - bodyA.vx;
-		final float relVelY = bodyB.vy - bodyA.vy;
-
-		final float dotVelNor = Vector2f.dot(relVelX, relVelY, manifold.mtv.x, manifold.mtv.y);
-
-		if (dotVelNor > 0.f)
-			return;
-
-		final float minRestitution = Math.min(bodyA.restitution(), bodyB.restitution());
-		float j = -(1.f + minRestitution) * dotVelNor;
-
-		j /= (bodyA.invMass() + bodyB.invMass());
-
-		final float impulseX = j * manifold.mtv.x;
-		final float impulseY = j * manifold.mtv.y;
-
-		bodyA.vx -= impulseX * bodyA.invMass();
-		bodyA.vy -= impulseY * bodyA.invMass();
-
-		bodyB.vx += impulseX * bodyB.invMass();
-		bodyB.vy += impulseY * bodyB.invMass();
 	}
 
 	// --------------------------------------
@@ -241,17 +249,27 @@ public class PhysicsWorld {
 		return mBodies.get(bodyIndex);
 	}
 
-	public void createContacts(int amount) {
-		amount = MathHelper.clampi(amount, 1, 128);
+	private CollisionPair getFreeCollisionPair() {
+		if (mCollisionPairPool.size() > 0) {
+			final var obj = mCollisionPairPool.removeFirst();
+			return obj;
+		}
+
+		enlargeCollisionPairPool(8);
+		final var obj = mCollisionPairPool.removeFirst();
+		return obj;
+	}
+
+	private void enlargeCollisionPairPool(final int amount) {
 		for (int i = 0; i < amount; i++) {
-			mContactPool.add(new ContactManifold());
+			mCollisionPairPool.addLast(new CollisionPair());
 		}
 	}
 
-	public ContactManifold getFreeContactManifold() {
-		if (mContactPool.size() == 0)
-			createContacts(16);
-
-		return mContactPool.remove();
+	private void returnCollisionPair(CollisionPair obj) {
+		obj.bodyAUid = 0;
+		obj.bodyBUid = 0;
+		mCollisionPairPool.addLast(obj);
 	}
+
 }
