e67e6d6ca90d250515ad1760958cb5f06c937982
LintfordPickle <jonnohampson@gmail.com>
2023-03-20 22:20:22 +0100

    Added date to GameVersion

--- src/net/lintford/library/GameVersion.java
+++ src/net/lintford/library/GameVersion.java
@@ -8,7 +8,8 @@ public class GameVersion {
 
 	public static final int APP_VERSION_MAJ = 0;
 	public static final int APP_VERSION_MIN = 1;
-	public static final int APP_VERSION_BUILD = 1102;
+	public static final int APP_VERSION_BUILD = 1;
+	public static final String APP_DATE = "032023";
 
 	public static final int APP_ITERATION = 1;
 
@@ -23,24 +24,26 @@ public class GameVersion {
 
 	/** Returns the game version as a string */
 	private static String getGameVersion() {
-		return APP_VERSION_MAJ + DELIMITOR + APP_VERSION_MIN + DELIMITOR + APP_VERSION_BUILD;
+		return APP_VERSION_MAJ + DELIMITOR + APP_VERSION_MIN + DELIMITOR + APP_VERSION_BUILD + DELIMITOR + APP_DATE;
 	}
 
 	/** Checks if the given version string matches the version of the game as defined in {@link GameVersion} */
 	public static boolean checkVersion(String versionString) {
 		int mj, mi, bu = 0;
+		String buildDate = null;
 
 		final var lParts = versionString.split(DELIMITOR);
 
-		if (lParts.length != 3) {
+		if (lParts.length != 4) {
 			System.err.println("Not a valid app version (not enough parts)");
 			return false;
 		}
 
-		mj = Integer.parseInt(lParts[0]);
-		mi = Integer.parseInt(lParts[1]);
-		bu = Integer.parseInt(lParts[2]);
+		mj = Integer.parseInt(lParts[0]); // Major
+		mi = Integer.parseInt(lParts[1]); // Minor
+		bu = Integer.parseInt(lParts[2]); // Hotfix
+		buildDate = lParts[3]; // Date
 
-		return APP_VERSION_MAJ == mj && APP_VERSION_MIN == mi && APP_VERSION_BUILD == bu;
+		return APP_VERSION_MAJ == mj && APP_VERSION_MIN == mi && APP_VERSION_BUILD == bu && APP_DATE.equals(buildDate);
 	}
 }
