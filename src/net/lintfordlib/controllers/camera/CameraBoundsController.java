5a2fb5e8c59b5f201b8a6b62a7d0313ac3c74bf4
LintfordPickle <jonnohampson@gmail.com>
2023-12-06 23:48:00 +0100

    Bounds in CameraController can be disabled

--- src/net/lintfordlib/controllers/camera/CameraBoundsController.java
+++ src/net/lintfordlib/controllers/camera/CameraBoundsController.java
@@ -12,7 +12,7 @@ public class CameraBoundsController extends BaseController {
 	// Constants
 	// ---------------------------------------------
 
-	public static final String CONTROLLER_NAME = "Editor Camera Controller";
+	public static final String CONTROLLER_NAME = "Camera Bounds Controller";
 
 	// ---------------------------------------------
 	// Variables
@@ -25,7 +25,8 @@ public class CameraBoundsController extends BaseController {
 	public float mZoomFactor;
 	public float mZoomVelocity;
 
-	// TODO: separate bounds
+	public boolean mLimitBounds;
+
 	public float sceneWidthInPx;
 	public float sceneHeightInPx;
 
@@ -40,6 +41,14 @@ public class CameraBoundsController extends BaseController {
 	public void setBounds(float sceneWidthInPx, float sceneHeightInPx) {
 		this.sceneWidthInPx = sceneWidthInPx;
 		this.sceneHeightInPx = sceneHeightInPx;
+	}
+
+	public void limitBounds(boolean limitBounds) {
+		mLimitBounds = limitBounds;
+	}
+
+	public boolean limitBounds() {
+		return mLimitBounds;
 	}
 
 	@Override
@@ -57,9 +66,10 @@ public class CameraBoundsController extends BaseController {
 		mGameCamera = camera;
 		mCameraPositionRef = mGameCamera.getPosition();
 
-		// TODO: DEBUG
 		sceneWidthInPx = 2048;
 		sceneHeightInPx = 2048;
+
+		mLimitBounds = true;
 	}
 
 	// ---------------------------------------------
@@ -76,24 +86,26 @@ public class CameraBoundsController extends BaseController {
 		if (mGameCamera == null)
 			return;
 
-		final var lCamWidth = mGameCamera.getWidth();
-		final var lCamHeight = mGameCamera.getHeight();
+		if (mLimitBounds) {
 
-		// TODO: Lots more work here (need to consider the zoom)
+			final var lCamWidth = mGameCamera.getWidth();
+			final var lCamHeight = mGameCamera.getHeight();
 
-		// ensure camera doesn't go beyond scene extents
-		if (sceneWidthInPx != 0) {
-			if (mCameraPositionRef.x - lCamWidth * .5f < -sceneWidthInPx * 0.5f)
-				mGameCamera.setPosition(-sceneWidthInPx * 0.5f + lCamWidth * .5f, mCameraPositionRef.y);
-			if (mCameraPositionRef.x + lCamWidth * .5f > sceneWidthInPx * 0.5f)
-				mCameraPositionRef.x = sceneWidthInPx * 0.5f - lCamWidth * .5f;
-		}
+			// ensure camera doesn't go beyond scene extents
+			if (sceneWidthInPx != 0) {
+				if (mCameraPositionRef.x - lCamWidth * .5f < -sceneWidthInPx * 0.5f)
+					mCameraPositionRef.x = -sceneWidthInPx * 0.5f + lCamWidth * .5f;
+				if (mCameraPositionRef.x + lCamWidth * .5f > sceneWidthInPx * 0.5f)
+					mCameraPositionRef.x = sceneWidthInPx * 0.5f - lCamWidth * .5f;
+			}
 
-		if (sceneHeightInPx != 0) {
-			if (mCameraPositionRef.y - lCamHeight * .5f < -sceneHeightInPx * 0.5f)
-				mCameraPositionRef.y = -sceneHeightInPx * 0.5f + lCamHeight * .5f;
-			if (mCameraPositionRef.y + lCamHeight * .5f > sceneHeightInPx * 0.5f)
-				mCameraPositionRef.y = sceneHeightInPx * 0.5f - lCamHeight * .5f;
+			if (sceneHeightInPx != 0) {
+				if (mCameraPositionRef.y - lCamHeight * .5f < -sceneHeightInPx * 0.5f)
+					mCameraPositionRef.y = -sceneHeightInPx * 0.5f + lCamHeight * .5f;
+				if (mCameraPositionRef.y + lCamHeight * .5f > sceneHeightInPx * 0.5f)
+					mCameraPositionRef.y = sceneHeightInPx * 0.5f - lCamHeight * .5f;
+
+			}
 		}
 
 	}
@@ -104,5 +116,11 @@ public class CameraBoundsController extends BaseController {
 
 	public void zoomIn(float zoomFactor) {
 		mGameCamera.setZoomFactor(zoomFactor);
+	}
+
+	public void resetCameraPosition() {
+		mGameCamera.setZoomFactor(1.f);
+		mGameCamera.setPosition(0.f, 0.f);
+
 	}
 }
