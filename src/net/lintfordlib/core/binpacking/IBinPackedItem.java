0373f06642a8758812ae9413860b78233028e557
LintfordPickle <jonnohampson@gmail.com>
2024-03-05 21:10:29 +0100

    Bin packed items provide world center position (x,y)

--- src/net/lintfordlib/core/binpacking/IBinPackedItem.java
+++ src/net/lintfordlib/core/binpacking/IBinPackedItem.java
@@ -1,10 +1,19 @@
 package net.lintfordlib.core.binpacking;
 
+import java.util.List;
+
 import net.lintfordlib.core.geometry.Rectangle;
+import net.lintfordlib.core.maths.Vector4f;
 
 public interface IBinPackedItem {
 
 	Rectangle world();
+
+	List<Vector4f> worldVerticesUv();
+
+	float worldCenterX();
+
+	float worldCenterY();
 
 	float binPositionX();
 
