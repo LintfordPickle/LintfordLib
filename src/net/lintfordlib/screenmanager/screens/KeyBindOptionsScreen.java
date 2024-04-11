84388ac2b1b5ef3ec2a633e82afce13a90ac36a4
LintfordPickle <jonnohampson@gmail.com>
2024-04-11 23:01:34 +0200

    fixed compile issue (missing interface)

--- src/net/lintfordlib/screenmanager/screens/KeyBindOptionsScreen.java
+++ src/net/lintfordlib/screenmanager/screens/KeyBindOptionsScreen.java
@@ -13,7 +13,7 @@ import net.lintfordlib.screenmanager.layouts.BaseLayout;
 import net.lintfordlib.screenmanager.layouts.HorizontalLayout;
 import net.lintfordlib.screenmanager.layouts.ListLayout;
 
-public class KeyBindOptionsScreen extends MenuScreen implements IValidateScreen {
+public class KeyBindOptionsScreen extends MenuScreen {
 
 	// --------------------------------------
 	// Constants
@@ -125,16 +125,4 @@ public class KeyBindOptionsScreen extends MenuScreen implements IValidateScreen 
 		}
 	}
 
-	// --------------------------------------
-	// Callback-Methods
-	// --------------------------------------
-
-	@Override
-	public void validateScreen(MenuEntry e) {
-		// final var lInputManager = screenManager().core().input();
-		// final var lEventActionManager = lInputManager.eventActionManager();
-
-		// TODO: One of the key binds has just changed, so check the validity of the new binds.
-
-	}
 }
