52c89788904de2e57e67c9fddcf314f86df2d612
LintfordPickle <jonnohampson@gmail.com>
2024-12-20 23:00:17 +0100

    added definition for ENTRY_OVER event

--- src/net/lintfordlib/screenmanager/ConstantsScreenManagerAudio.java
+++ src/net/lintfordlib/screenmanager/ConstantsScreenManagerAudio.java
@@ -3,10 +3,14 @@ package net.lintfordlib.screenmanager;
 public class ConstantsScreenManagerAudio {
 
 	public static final String SCREENMANAGER_AUDIO_ENTRY_SELECTED = "SOUND_MENU_CLICK";
+	public static final String SCREENMANAGER_AUDIO_ENTRY_OVER = "SOUND_MENU_OVER";
 	public static final String SCREENMANAGER_AUDIO_ENTRY_OPEN = "SOUND_MENU_CLICK";
 	public static final String SCREENMANAGER_AUDIO_ENTRY_CLOSE = "SOUND_MENU_CLICK";
 	public static final String SCREENMANAGER_AUDIO_ENTRY_TICK = "SOUND_MENU_CLICK";
 
 	public static final String SCREENMANAGER_AUDIO_ENTRY_NAVIGATION_UP = "SOUND_MENU_NAV_UP";
 	public static final String SCREENMANAGER_AUDIO_ENTRY_NAVIGATION_DOWN = "SOUND_MENU_NAV_DOWN";
+
+	private ConstantsScreenManagerAudio() {
+	}
 }
