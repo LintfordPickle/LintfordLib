6cbc7b486d1f70b9ad71a960e9e31a2100f431a5
LintfordPickle <jonnohampson@gmail.com>
2025-12-13 13:19:11 +0100

    first pass at a 'global' MenuStyles class. This will be used to set defaults to MenuEntries.

---
+++ src/net/lintfordlib/screenmanager/MenuStyles.java
@@ -0,0 +1,21 @@
+package net.lintfordlib.screenmanager;
+
+public class MenuStyles {
+
+	private MenuStyles() {
+	}
+
+	public static float entryDesiredWidth = 200;
+	public static float entryDesiredHeight = 25;
+
+	public static float marginLeft = 5;
+	public static float marginRight = 5;
+	public static float marginBottom = 5;
+	public static float marginTop = 5;
+
+	public static float paddingLeft = 0;
+	public static float paddingRight = 0;
+	public static float paddingBottom = 5;
+	public static float paddingTop = 5;
+
+}
