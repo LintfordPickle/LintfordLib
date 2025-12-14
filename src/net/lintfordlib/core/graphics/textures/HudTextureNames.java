1c9f227646fafdd91e90b3354707f5fb0ad1b950
LintfordPickle <jonnohampson@gmail.com>
2025-12-14 22:35:35 +0100

    raw assets updated

---
+++ src/net/lintfordlib/core/graphics/textures/HudTextureNames.java
@@ -0,0 +1,11 @@
+package net.lintfordlib.core.graphics.textures;
+
+public class HudTextureNames {
+
+	public static final int TEXTURE_HIDE_LAYER = 0;
+	public static final int TEXTURE_SAVE_LAYER = 1;
+	public static final int TEXTURE_SET_LAYER_OFF = 2;
+	public static final int TEXTURE_SET_LAYER_ON = 3;
+	public static final int TEXTURE_SHOW_LAYER = 4;
+
+}
